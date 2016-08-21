b59fe267431526754ea9547e565eda38854fd253
Anna <annasborysova@gmail.com>
2016-08-21 16:48:00 +0200

    add utterances and basic intents responses

--- app.rb
+++ app.rb
@@ -30,12 +30,38 @@ post '/echo' do
       end_session: false
     )
   when 'IntentRequest'
+    end_session = false
+    text = ""
     case params[:request][:intent][:name]
     when 'RawText'
       text = params[:request][:intent][:slots][:Text][:value]
       end_session = (text == 'end')
-      Response.speech(text, end_session: end_session)
+    when 'ReadAgenda'
+      text = "reading agenda"
+    when 'RecordAgendaItem'
+      agendaItem = params[:request][:intent][:slots][:AgendaItem][:value]
+      text = "recording agenda item #{agendaItem}"
+    when 'ReadActionList'
+      text = "reading action list"
+    when 'RecordActionItem'
+      actionItem = params[:request][:intent][:slots][:ActionItem][:value]
+      text = "recording action item #{actionItem}"
+    when 'RecordMotion'
+      motion = params[:request][:intent][:slots][:Motion][:value]
+      text = "recording motion #{motion}"
+    when 'RecordMotionResult'
+      motionResult = params[:request][:intent][:slots][:MotionResult][:value]
+      text = "record motion result #{motionResult}"
+    when 'AddAttendee'
+      attendee = params[:request][:intent][:slots][:Attendee][:value]
+      text = "add attendee #{attendee}"
+    when 'RecordNote'
+      note = params[:request][:intent][:slots][:Note][:value]
+      text = "record note #{note}"
+    when 'NextAgendaItem'
+      text = "moving along now..."
     end
+    Response.speech(text, end_session: end_session)
   when 'SessionEndedRequest'
   end || {}
 
