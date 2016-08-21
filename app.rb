8f6cfc81300193a2f6e24438a19b65a57c8bc5eb
Kieren Davies <kieren@kdavi.es>
2016-08-21 11:35:45 +0200

    Refactor response building

--- app.rb
+++ app.rb
@@ -1,12 +1,20 @@
 require 'sinatra'
 require 'sinatra/json'
-require 'sinatra/reloader' if development?
 
 require 'json'
 require 'active_support'
 require 'active_support/core_ext'
 
 require 'pp'
+require 'pry'
+require 'set'
+
+require './lib/response'
+
+require 'sinatra/reloader' if development?
+also_reload './lib/response'
+
+$sessions = Set.new
 
 get '/' do
   'It works!'
@@ -16,15 +24,30 @@ post '/echo' do
   request.body.rewind
   @params = JSON.parse(request.body.read).with_indifferent_access
 
-  # case params[]
+  if !$sessions.include? params[:session][:sessionId]
+    logger.info "\n===== new session ====="
+    $sessions << params[:session][:sessionId]
+  end
 
-  json({
-    response: {
-      outputSpeech: {
-        type: 'PlainText',
-        text: 'recording meeting minutes, starting now'
-      },
-      shouldEndSession: true
-    }
-  })
+  logger.info "Request:\n#{JSON.pretty_generate params}"
+
+  response = case params[:request][:type]
+  when 'LaunchRequest'
+    Response.speech(
+      # 'recording meeting minutes, starting now',
+      'starting',
+      end_session: false
+    )
+  when 'IntentRequest'
+    case params[:request][:intent][:name]
+    when 'RawText'
+      text = params[:request][:intent][:slots][:Text][:value]
+      end_session = (text == 'end')
+      Response.speech(text, end_session: end_session)
+    end
+  when 'SessionEndedRequest'
+  end
+
+  logger.info "Response:\n#{JSON.pretty_generate response}"
+  json response
 end
---
+++ lib/response.rb
@@ -0,0 +1,101 @@
+# {
+#   version: "string",
+#   sessionAttributes: {
+#     string: object
+#   },
+#   response: {
+#     outputSpeech: {
+#       type: "string",
+#       text: "string",
+#       ssml: "string"
+#     },
+#     card: {
+#       type: "string",
+#       title: "string",
+#       content: "string",
+#       text: "string",
+#       image: {
+#         smallImageUrl: "string",
+#         largeImageUrl: "string"
+#       }
+#     },
+#     reprompt: {
+#       outputSpeech: {
+#         type: "string",
+#         text: "string",
+#         ssml: "string"
+#       }
+#     },
+#     shouldEndSession: boolean
+#   }
+# }
+
+module Response
+  SSML_TAGS = %w(audio break p phoneme s say-as speak w)
+
+  def self.speech speech, **opts
+    if speech.is_a? String
+      type = :PlainText
+    else
+      type = :SSML
+      speech = build_ssml speech
+    end
+
+    response = {
+      response: {
+        outputSpeech: {
+          type: type,
+          text: speech
+        },
+        shouldEndSession: opts.fetch(:end_session, true)
+      }
+    }
+    if opts[:reprompt]
+      response[:response][:reprompt] = {
+        outputSpeech: {
+          type: 'PlainText',
+          text: opts[:reprompt] || speech
+        },
+      }
+    end
+
+    response
+  end
+
+  def self.build_ssml obj, root=true
+    if root
+      "<speak>#{build_ssml obj, false}</speak>"
+    elsif obj.is_a? String
+      "<s>#{obj}</s>"
+    elsif obj.is_a? Array
+      obj.map { |o| build_ssml o, false }.join ' '
+    elsif obj.is_a? Hash
+      key = obj.keys.first
+      val = obj[key]
+      case key
+      when :audio
+        "<audio src=\"#{val}\"/>"
+      when :break
+        if val =~ /\d+m?s/
+          "<break time=\"#{val}\"/>"
+        else
+          "<break strength=\"#{val}\"/>"
+        end
+      when :p
+        "<p>#{val}</p>"
+      when :phoneme
+        "<phoneme alphabet=\"#{val.fetch(:alphabet, 'x-sampa')}\" ph=\"#{val[:ph]}\">#{val[:text]}</phoneme>"
+      when :s
+        "<s>#{val}</s>"
+      when :say_as
+        "<say-as interpret-as=\"#{val[:interpret_as]}\" format=\"#{val[:format]}\">#{val[:text]}</say-as>"
+      when :w
+        "<w role=\"ivona:#{val[:role].upcase}\">#{val[:text]}</w>"
+      else
+        raise 'unsupported key'
+      end
+    else
+      raise 'unsupported type'
+    end
+  end
+end
